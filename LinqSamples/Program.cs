b476679db8aaff210fb889af7b8a6662bf50f4fe
Ismail NORRI <ismail.norri.24@gmail.com>
2020-04-17 22:02:05 +0000

    Création d'une app console, Method :ShowLargesFileWithoutLinq()

---
+++ LinqSamples/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowLargesFilesWithoutLinq();
+        }
+
+        private static void ShowLargesFilesWithoutLinq()
+        {
+            string path = @"C:\Windows";
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            FileInfo[] files = directoryInfo.GetFiles();
+
+            Array.Sort(files,new FileInfoComparer());
+
+            for (int i = 0; i < 5; i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name, -20} : {file.Length, 10:N0}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
