453f03576556a93781621ab3e15cb789d0e2e0dd
Ismail NORRI <ismail.norri.24@gmail.com>
2020-04-18 08:15:57 +0000

    Some Query method, Query syntax, Lambda expression and anonymous method with Func<>

---
+++ LinqSamples/Features/myLinq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Features.Linq
+{
+    public static class myLinq
+    {
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+            foreach (var item in sequence)
+            {
+                count++;
+            }
+            return count;
+        }
+       
+    }
+}
